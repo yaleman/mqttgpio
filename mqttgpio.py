384be75a50151ad9c19575255a82354a318297ba
yaleman <yaleman@ricetek.net>
2019-12-03 22:59:25 +1000

    more pylint cleanup

--- mqttgpio.py
+++ mqttgpio.py
@@ -132,25 +132,25 @@ class GPIOSwitch(object):
                 self.pin_io.on()
             else:
                 self.pin_io.off()
-            self.logger.debug(f"{self.name}:{self.pin} (GPIO) = {state}")
+            self.logger.debug("%s:%s (GPIO) = %s", self.name, self.pin, state)
         else:
-            self.logger.debug(f"{self.name}:{self.pin} (dev-mode) = {state}")
+            self.logger.debug("%s:%s (dev-mode) = %s", self.name, self.pin, state)
         self.state = state
         self.announce_state()
 
     def handle_command(self, payload):
         """ takes actions based on incoming commands """
-        self.logger.debug(f"{self.name}.handle_command({payload})")
+        self.logger.debug("%s.handle_command(%s)", self.name, payload)
         if payload == b'ON':
             self._set_state(True)
         elif payload == b'OFF':
             self._set_state(False)
         else:
-            logger.WARN(f"{self.name}.handle_command({payload}) is weird - should match '(ON|OFF)'")
+            logger.WARN("%s.handle_command(%s) is weird - should match '(ON|OFF)'", self.name, payload)
 
 def on_connect(client_object, userdata, flags, result_code): # noqa: pylint: disable=unused-argument
     """The callback for when the client receives a CONNACK response from the server."""
-    logger.info(f"Connected with result code %s", result_code)
+    logger.info("Connected with result code %s", result_code)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -167,13 +167,13 @@ def on_message(client_object, userdata, msg): # noqa: pylint: disable=unused-arg
             device_object.handle_command(msg.payload)
             matched = True
     if not matched and not msg.topic.startswith('$SYS'):
-        logger.info(f"Command for unknown device: {msg.topic}={str(msg.payload)}")
+        logger.info("Command for unknown device: %s=%s", msg.topic, msg.payload)
 
 if __name__ == '__main__':
 
-    mqtt_qos = cfg.getint("MQTT", 'MQTTQOS', fallback=2)
-    mqtt_broker = cfg.get("MQTT", "MQTTBroker", fallback='localhost')
-    mqtt_port = cfg.getint("MQTT", "MQTTPort", fallback=1883)
+    MQTT_QOS = cfg.getint("MQTT", 'MQTTQOS', fallback=2)
+    MQTT_BROKER = cfg.get("MQTT", "MQTTBroker", fallback='localhost')
+    MQTT_PORT = cfg.getint("MQTT", "MQTTPort", fallback=1883)
 
     client = mqtt.Client()
     # callback functions for MQTT
@@ -181,15 +181,17 @@ if __name__ == '__main__':
     client.on_message = on_message
 
 
-    logger.debug(f"Connecting to mqtt://{mqtt_broker}:{mqtt_port}")
+    logger.debug("Connecting to mqtt://%s:%s", MQTT_BROKER, MQTT_PORT)
     while True:
         try:
-            client.connect(mqtt_broker, mqtt_port, 60)
+            client.connect(MQTT_BROKER, MQTT_PORT, 60)
             break # break out of the "retry until it works" loop
         except ConnectionRefusedError:
-            logger.info(f"Unable to connect to mqtt://{mqtt_broker}:{mqtt_port}, connection refused. Sleeping for 60 seconds.") # noqa: pylint: disable=line-too-long
+            logger.info("Unable to connect to mqtt://%s:%s, connection refused. Sleeping for 60 seconds.",
+                        MQTT_BROKER,
+                        MQTT_PORT)
             time.sleep(60)
-    logger.debug(f"Connected to mqtt://{mqtt_broker}:{mqtt_port}")
+    logger.debug("Connected to mqtt://%s:%s", MQTT_BROKER, MQTT_PORT)
 
     # create the device/pin associations from the config file
     ACTIVE_DEVICES = []
@@ -199,7 +201,12 @@ if __name__ == '__main__':
                 # look for a device_state option
                 state = cfg.getboolean('Devices', f"{name}_default", fallback=False)
                 logger.debug("Creating %s:%s (%s)", name, pin, state)
-                ACTIVE_DEVICES.append(GPIOSwitch(name=name, pin=pin, client=client, qos=mqtt_qos, initial_state=state))
+                ACTIVE_DEVICES.append(GPIOSwitch(name=name,
+                                                 pin=pin,
+                                                 client=client,
+                                                 qos=MQTT_QOS,
+                                                 initial_state=state
+                                                 ))
 
     logger.debug("Starting the MQTT thread")
     client.loop_start()
