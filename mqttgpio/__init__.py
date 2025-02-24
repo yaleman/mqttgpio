420c31219fd7f4bae27752b6fe2b91c9ba54c7a8
James Hodgkinson <james@terminaloutcomes.com>
2025-02-25 09:40:15 +1000

    v0.0.2 - moving to uv, adding type data

--- mqttgpio/__init__.py
+++ mqttgpio/__init__.py
@@ -1,23 +1,58 @@
-""" mqttgpio - a shim between MQTT and GPIO on raspberry Pi """
+"""mqttgpio - a shim between MQTT and GPIO on raspberry Pi"""
 
+from configparser import ConfigParser
 import json
 import logging
 
-import gpiozero # type: ignore
-import paho.mqtt.client as mqtt # type: ignore
+import gpiozero  # type: ignore
+import paho.mqtt.client as mqtt
+from paho.mqtt.client import MQTTMessageInfo
 
-class GPIOSwitch(): #pylint: disable=too-many-instance-attributes
-    """ a single pin controller """
-    def __init__(self, name: str,
-                 pin: int,
-                 client: mqtt.Client,
-                 qos: int,
-                 logging_object: logging.Logger,
-                 initial_state: bool = False,
-                 mock_pins: bool = False,
-                ): #pylint: disable=too-many-arguments
+CONFIG_FILES = ["/etc/mqttgpio.conf", "./mqttgpio.conf", "/opt/mqttgpio/mqttgpio.conf"]
+
+LOG_LEVELS = {
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+}
+
+
+def load_config(log_object: logging.Logger) -> ConfigParser:
+    config = ConfigParser()
+    parsed_files = config.read(CONFIG_FILES)
+
+    LOG_LEVEL = config.get("Default", "logging", fallback="info")
+
+    if LOG_LEVEL.lower() in LOG_LEVELS:
+        log_object.setLevel(LOG_LEVELS[LOG_LEVEL.lower()])
+    else:
+        log_object.setLevel(logging.DEBUG)
+        log_object.debug(
+            "Configuration file had a misconfigured 'logging' setting (%s) - setting to DEBUG",
+            LOG_LEVEL,
+        )  # pylint: disable=line-too-long
+
+    log_object.info("Loaded configuration from: %s", ",".join(parsed_files))
+
+    return config
+
+
+class GPIOSwitch:
+    """a single pin controller"""
+
+    def __init__(
+        self,
+        name: str,
+        pin: int,
+        client: mqtt.Client,
+        qos: int,
+        logging_object: logging.Logger,
+        initial_state: bool = False,
+        mock_pins: bool = False,
+    ) -> None:
         self.name = name
-        self.device_class = 'switch'
+        self.device_class = "switch"
         self.client = client
         self.mqtt_qos = qos
         self.logger = logging_object
@@ -25,70 +60,70 @@ class GPIOSwitch(): #pylint: disable=too-many-instance-attributes
         if mock_pins:
             self.pin_io = gpiozero.Device.pin_factory.pin(pin)
         else:
-            self.pin_io = gpiozero.LED(pin) # pylint: disable=undefined-variable
+            self.pin_io = gpiozero.LED(pin)  # pylint: disable=undefined-variable
 
         # might as well say hello on startup
         self.announce_config()
         self._set_state(initial_state)
 
-    def str_state(self):
-        """ returns the state in the home assistant version """
+    def str_state(self) -> str:
+        """returns the state in the home assistant version"""
         if self.state:
             return "ON"
         return "OFF"
 
-    def config_topic(self):
-        """ returns the config topic """
+    def config_topic(self) -> str:
+        """returns the config topic"""
         return f"homeassistant/{self.device_class}/{self.name}/config"
 
-    def state_topic(self):
-        """ returns the state topic as a string """
+    def state_topic(self) -> str:
+        """returns the state topic as a string"""
         return f"{self.name}/state"
 
-    def command_topic(self):
-        """ returns the command topic as a string """
+    def command_topic(self) -> str:
+        """returns the command topic as a string"""
         return f"{self.name}/cmnd"
 
-    def _publish(self, topic, payload):
-        """ publishes a message """
-        return self.client.publish(topic,
-                                   payload,
-                                   qos=self.mqtt_qos,
-                                   )
+    def _publish(self, topic: str, payload: str) -> MQTTMessageInfo:
+        """publishes a message"""
+        return self.client.publish(
+            topic,
+            payload,
+            qos=self.mqtt_qos,
+        )
 
-
-    def announce_config(self):
-        """ sends the MQTT message to configure
-            home assistant
+    def announce_config(self) -> None:
+        """sends the MQTT message to configure
+        home assistant
         """
         payload = {
-            'name' : self.name,
-            'state_topic' : self.state_topic(),
-            'command_topic' : self.command_topic(),
-            "val_tpl" : '{{value_json.POWER}}',
+            "name": self.name,
+            "state_topic": self.state_topic(),
+            "command_topic": self.command_topic(),
+            "val_tpl": "{{value_json.POWER}}",
         }
         self.logger.debug(
             "%s.announce_config(%s)",
             self.name,
             payload,
-            )
+        )
         self._publish(
             self.config_topic(),
             payload=json.dumps(payload),
-            )
+        )
 
-    def announce_state(self):
-        """ sends the MQTT message about the current state """
-        payload = {'POWER' : self.str_state()}
+    def announce_state(self) -> None:
+        """sends the MQTT message about the current state"""
+        payload = {"POWER": self.str_state()}
 
         self.logger.debug("%s.announce_state(%s)", self.name, payload)
         self._publish(self.state_topic(), payload=json.dumps(payload))
 
-    def _set_state(self, state):
-        """ Does a few things:
-            - sets the internal state variable
-            - sets the GPIO
-            - announces via MQTT the current state
+    def _set_state(self, state: bool) -> None:
+        """Does a few things:
+        - sets the internal state variable
+        - sets the GPIO
+        - announces via MQTT the current state
         """
         if self.mock_pins:
             if state:
@@ -105,12 +140,16 @@ class GPIOSwitch(): #pylint: disable=too-many-instance-attributes
         self.state = state
         self.announce_state()
 
-    def handle_command(self, payload):
-        """ takes actions based on incoming commands """
+    def handle_command(self, payload: bytes) -> None:
+        """takes actions based on incoming commands"""
         self.logger.debug("%s.handle_command(%s)", self.name, payload)
-        if payload == b'ON':
+        if payload == b"ON":
             self._set_state(True)
-        elif payload == b'OFF':
+        elif payload == b"OFF":
             self._set_state(False)
         else:
-            self.logger.WARN("%s.handle_command(%s) is weird - should match '(ON|OFF)'", self.name, payload) # pylint: disable=line-too-long
+            self.logger.warning(
+                "%s.handle_command(%s) is weird - should match '(ON|OFF)'",
+                self.name,
+                payload,
+            )  # pylint: disable=line-too-long
--- mqttgpio/__main__.py
+++ mqttgpio/__main__.py
@@ -8,51 +8,61 @@ Discovery docs: https://www.home-assistant.io/docs/mqtt/discovery/
 Switch docs: https://developers.home-assistant.io/docs/en/entity_switch.html
 
 """
+
 import logging
 from os import uname
+import socket
 import time
 import sys
-import configparser
+from typing import Any
 
 try:
-    import gpiozero # type: ignore
-    import paho.mqtt.client as mqtt # type: ignore
-    import schedule # type: ignore
+    import gpiozero  # type: ignore
+    import paho.mqtt.client as mqtt
+    from paho.mqtt.enums import CallbackAPIVersion
+    from paho.mqtt.client import Client, MQTTMessage
+    from paho.mqtt.reasoncodes import ReasonCode
+    import schedule
 except ImportError as import_error:
     sys.exit(f"Package import failure: {import_error}")
 
-from . import GPIOSwitch
+from mqttgpio import GPIOSwitch, load_config
 
-
-LOG_OBJECT = logging.getLogger('mqttcontroller') #pylint: disable: invalid-name
+LOG_OBJECT = logging.getLogger("mqttcontroller")  # pylint: disable: invalid-name
 LOG_OBJECT.addHandler(logging.StreamHandler())
 
+CONFIG = load_config(LOG_OBJECT)
 
 
-CONFIG = configparser.ConfigParser()
-CONFIGFILES = ['/etc/mqttgpio.conf', './mqttgpio.conf', '/opt/mqttgpio/mqttgpio.conf']
-PARSED_FILES = CONFIG.read(CONFIGFILES)
+class FailedToConnect(BaseException):
+    """Raised when the MQTT connection fails"""
 
-LOG_LEVEL = CONFIG.get('Default', 'logging', fallback='info')
-
-LOG_LEVELS = {'info' : logging.INFO,
-              'debug' : logging.DEBUG,
-              'warning' : logging.WARNING,
-              'error' : logging.ERROR
-             }
-if LOG_LEVEL in LOG_LEVELS:
-    LOG_OBJECT.setLevel(LOG_LEVELS[LOG_LEVEL])
-else:
-    LOG_OBJECT.setLevel(logging.DEBUG)
-    LOG_OBJECT.debug("Configuration file had a misconfigured 'logging' setting (%s) - setting to DEBUG", LOG_LEVEL) #pylint: disable=line-too-long
-
-LOG_OBJECT.info("Loaded configuration from: %s", ','.join(PARSED_FILES))
+    def __init__(self, reason_code: ReasonCode) -> None:
+        super().__init__(f"Failed to connect to MQTT, reason code: {reason_code}")
 
 
-
-def mqtt_on_connect(client_object, userdata, flags, result_code): # noqa: pylint: disable=unused-argument
+def mqtt_on_connect(
+    client_object: Client,
+    _userdata: Any,
+    _flags: dict[str, Any],
+    reason_code: ReasonCode,
+    _properties: dict[str, Any],
+) -> None:
     """The callback for when the client receives a CONNACK response from the server."""
-    LOG_OBJECT.info("Connected with result code %s", result_code)
+
+    if reason_code.getId(str(reason_code)) > 0:  # type: ignore[no-untyped-call]
+        LOG_OBJECT.error(
+            "Connection failed, reason code %s (%d)",
+            reason_code,
+            reason_code.getId(str(reason_code)),  # type: ignore[no-untyped-call]
+        )
+        raise FailedToConnect(reason_code)
+    else:
+        LOG_OBJECT.info(
+            "Connected with reason code %s (%d)",
+            reason_code,
+            reason_code.getId(str(reason_code)),  # type: ignore[no-untyped-call]
+        )
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -60,7 +70,8 @@ def mqtt_on_connect(client_object, userdata, flags, result_code): # noqa: pylint
     for device_object in ACTIVE_DEVICES:
         client_object.subscribe(device_object.command_topic())
 
-def mqtt_on_message(client_object, userdata, msg): # noqa: pylint: disable=unused-argument
+
+def mqtt_on_message(_client_object: Client, _userdata: Any, msg: MQTTMessage) -> None:
     """The callback for when a PUBLISH message is received from the server."""
     matched = False
     for device_object in ACTIVE_DEVICES:
@@ -68,71 +79,82 @@ def mqtt_on_message(client_object, userdata, msg): # noqa: pylint: disable=unuse
             LOG_OBJECT.info("Command to %s : %s", device_object.name, msg.payload)
             device_object.handle_command(msg.payload)
             matched = True
-    if not matched and not msg.topic.startswith('$SYS'):
+    if not matched and not msg.topic.startswith("$SYS"):
         LOG_OBJECT.info("Command for unknown device: %s=%s", msg.topic, msg.payload)
 
-if __name__ == '__main__':
 
-    MQTT_QOS = CONFIG.getint("MQTT", 'MQTTQOS', fallback=2)
-    MQTT_BROKER = CONFIG.get("MQTT", "MQTTBroker", fallback='localhost')
+if __name__ == "__main__":
+    MQTT_QOS = CONFIG.getint("MQTT", "MQTTQOS", fallback=2)
+    MQTT_BROKER = CONFIG.get("MQTT", "MQTTBroker", fallback="localhost")
     MQTT_PORT = CONFIG.getint("MQTT", "MQTTPort", fallback=1883)
 
-    MQTTCLIENT = mqtt.Client()
-    # callback functions for MQTT
-    MQTTCLIENT.on_connect = mqtt_on_connect
+    MQTTCLIENT = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
+    # callback functions for MQTT - type ignore is because of how the API is defined, V2 is different
+    MQTTCLIENT.on_connect = mqtt_on_connect  # type: ignore[assignment]
     MQTTCLIENT.on_message = mqtt_on_message
-
 
     LOG_OBJECT.debug("Connecting to mqtt://%s:%s", MQTT_BROKER, MQTT_PORT)
     while True:
         try:
             MQTTCLIENT.connect(MQTT_BROKER, MQTT_PORT, 60)
-            break # break out of the "retry until it works" loop
+            break  # break out of the "retry until it works" loop
+        except socket.gaierror as error:
+            LOG_OBJECT.error(
+                "Failed to resolve the MQTT broker hostname, sleeping for 60 seconds: %s",
+                error,
+            )
+            time.sleep(60)
         except ConnectionRefusedError:
-            LOG_OBJECT.info("Unable to connect to mqtt://%s:%s, connection refused. Sleeping for 60 seconds.", # pylint: disable=line-too-long
-                            MQTT_BROKER,
-                            MQTT_PORT,
-                            )
+            LOG_OBJECT.info(
+                "Unable to connect to mqtt://%s:%s, connection refused. Sleeping for 60 seconds.",  # pylint: disable=line-too-long
+                MQTT_BROKER,
+                MQTT_PORT,
+            )
             time.sleep(60)
     LOG_OBJECT.debug("Connected to mqtt://%s:%s", MQTT_BROKER, MQTT_PORT)
 
-
-    if 'arm' in uname().machine:
+    if "arm" in uname().machine:
         MOCK_PINS = False
     else:
-        from gpiozero.pins.mock import MockFactory # type: ignore
+        from gpiozero.pins.mock import MockFactory  # type: ignore
+
         gpiozero.Device.pin_factory = MockFactory()
         MOCK_PINS = True
         LOG_OBJECT.info("Not running on a Pi, using mock objects")
 
-
     # create the device/pin associations from the config file
-    ACTIVE_DEVICES = []
-    if CONFIG.has_section('Devices'):
-        for device_name, device_pin in CONFIG.items('Devices'):
+    ACTIVE_DEVICES: list[GPIOSwitch] = []
+    if CONFIG.has_section("Devices"):
+        for device_name, device_pin in CONFIG.items("Devices"):
             if not device_name.endswith("_default"):
                 # look for a device_state option
-                config_state = CONFIG.getboolean('Devices',
-                                                 f"{device_name}_default",
-                                                 fallback=False,
-                                                )
-                LOG_OBJECT.debug("Creating %s:%s (%s)", device_name, device_pin, config_state)
+                config_state = CONFIG.getboolean(
+                    "Devices",
+                    f"{device_name}_default",
+                    fallback=False,
+                )
+                LOG_OBJECT.debug(
+                    "Creating %s:%s (%s)", device_name, device_pin, config_state
+                )
                 try:
                     int_device_pin = int(device_pin)
                 except ValueError as error:
                     LOG_OBJECT.error(
                         "ValueError handling the configured device pin, bailing: %s",
                         error,
-                        )
+                    )
                     sys.exit(1)
-                ACTIVE_DEVICES.append(GPIOSwitch(name=device_name,
-                                                 pin=int(device_pin),
-                                                 client=MQTTCLIENT,
-                                                 qos=MQTT_QOS,
-                                                 initial_state=config_state,
-                                                 mock_pins=MOCK_PINS,
-                                                 logging_object=LOG_OBJECT,
-                                                 ))
+                ACTIVE_DEVICES.append(
+                    GPIOSwitch(
+                        name=device_name,
+                        pin=int(device_pin),
+                        client=MQTTCLIENT,
+                        qos=MQTT_QOS,
+                        initial_state=config_state,
+                        mock_pins=MOCK_PINS,
+                        logging_object=LOG_OBJECT,
+                    )
+                )
 
     LOG_OBJECT.debug("Starting the MQTT thread")
     MQTTCLIENT.loop_start()
@@ -149,7 +171,11 @@ if __name__ == '__main__':
             schedule.run_pending()
             time.sleep(1)
         # a hail-mary to keep it running :)
-        # pylint: disable=broad-except
+        except KeyboardInterrupt:
+            LOG_OBJECT.info("Exiting due to user input")
+            sys.exit(0)
         except Exception as error_message:
-            LOG_OBJECT.error("Failed to do something, sleeping for 60 seconds: %s", error_message)
+            LOG_OBJECT.error(
+                "Failed to do something, sleeping for 60 seconds: %s", error_message
+            )
             time.sleep(60)
